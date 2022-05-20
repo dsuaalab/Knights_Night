ed941146fd9ca16c8f4b9fe18ec46e3cfd4b553c
SeokMin <choiseokmin@naver.com>
2022-05-20 11:51:09 +0900

    Add enemyscene
    적 씬 추가

---
+++ Classes/EnemyScene.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class EnemyScene : public Scene
+{
+public:
+	static Scene* createScene();
+
+	virtual bool init();
+
+	//void createEnemy();
+
+	CREATE_FUNC(EnemyScene);
+	void setHealth(int newHealth);
+
+private:
+	int myHealth;
+	Sprite *body;
+	Sprite *weapon;
+
+	
+};
