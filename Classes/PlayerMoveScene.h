9f5eb24b863388a80ca9aba2526d9d0a95ee731a
Klay <min58417@gmail.com>
2022-05-20 10:30:29 +0900

    Update PlayerMove scene class
    Player의 PC기반 환경구축.

--- Classes/PlayerMoveScene.h
+++ Classes/PlayerMoveScene.h
@@ -1,14 +1,22 @@
 #pragma once
 #include "cocos2d.h"
+#include "Player.h"
 
 USING_NS_CC;
 
-class PlayerMove : public Scene
+class PlayerMoveScene : public Scene
 {
 public:
 	static Scene* createScene();
 
-	virtual bool init();
+	Player* player;
 
-	CREATE_FUNC(PlayerMove);
+	virtual bool init();
+	virtual void update(float delta);
+
+	// PC 테스트용 키입력 이벤트 함수
+	virtual void onKeyPressed(EventKeyboard::KeyCode keycode, Event* event);
+	virtual void onKeyReleased(EventKeyboard::KeyCode keycode, Event* event);
+
+	CREATE_FUNC(PlayerMoveScene);
 };
